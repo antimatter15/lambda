5a085e475b3e3997335040e780694f116c04a3f0
Joel Gustafson <joelg@mit.edu>
2016-10-27 22:23:57 -0400

    hi

--- src/graphics/flex2.js
+++ src/graphics/flex2.js
@@ -15,6 +15,7 @@ const default_domain = [0, 10];
 const slider_height = 36;
 const label_gutter = 36;
 const label_margin = 24;
+const check_width = 36;
 
 const fancy_f = '\u0192';
 
@@ -134,7 +135,7 @@ class Control {
         this.args = args;
         this.width = width;
         this.height = label_gutter;
-        this.rows = [];
+        this.env = {};
 
         this.label_container = parent.control_container.append('g');
         this.slider_container = parent.control_container.append('g');
@@ -145,10 +146,12 @@ class Control {
     }
     add_row(symbol, value) {
         this.height += slider_height;
-        this.rows.push(new Row(this, symbol, value));
+        this.env[symbol] = new Row(this, symbol, value);
     }
     add_point(name) {
-        this.rows.forEach(row => row.add_point(name));
+        // this.rows.forEach(row => row.add_point(name));
+        const point = new Point(name);
+        Object.keys(this.env).map(key => this.env[key]).forEach(point.append);
     }
     add_path(name) {
         this.rows.forEach(row => row.add_path(name));
@@ -164,12 +167,17 @@ class Control {
 
 class Row {
     constructor(parent, symbol, value, input) {
+        this.width = parent.width;
+        this.height = slider_height;
         this.symbol = symbol;
         this.value = value;
         this.input = input;
-        this.slider_width = parent.width - label_margin;
 
+        this.thing_margin = 8;
+        this.thing_width = check_width;
         this.things = [];
+
+        this.slider_width = this.width - label_margin - this.thing_width;
 
         this.row_container = parent.slider_container.append('g');
         this.label = this.row_container.append('text').text(this.symbol);
@@ -178,8 +186,21 @@ class Row {
             .attr('x2', this.slider_width).attr('y2', 0)
             .attr('stroke-width', 1)
             .attr('stroke', 'black');
+
+        this.track = this.row_container.append('g').attr('class', 'axis slider-axis');
+        this.scale = d3.scaleLinear().domain(default_domain).range([0, this.slider_width]).clamp(true);
+        this.axis = d3.axisBottom(this.scale).ticks(Math.floor(this.slider_width / tick_spacer));
+        this.track.call(this.axis);
+
+        this.handle = this.slider.append('circle')
+            .attr('r', 6)
+            .attr('cx', this.scale(value))
+            .attr('cy', 0);
+
         if (input) {
-            this.checkbox = this.row_container.append('foreignObject').attr('width', 32).attr('height', 32);
+            this.checkbox = this.row_container.append('foreignObject')
+                .attr('width', check_width).attr('height', this.height)
+                .attr('transform', `translate(${this.slider_width - check_width},0)`);
             const html = `<form><input type="checkbox" id="fixed-${symbol}"/></form>`;
             this.checkbox.append('xhtml:body').html(html);
             this.fixed = d3.select(`#fixed-${symbol}`).node();
@@ -188,36 +209,69 @@ class Row {
     is_fixed() {
         return this.checkbox && this.fixed.checked;
     }
-    add_thing(thing) {
-        const {width, height} = thing;
-        const element = this.row_container.append('foreignObject').attr('width', width).attr('height', height);
-        element.append('xhtml:body').html(thing.html(this));
+    add_thing(width, height, html) {
+        const element = this.row_container.append('foreignObject')
+            .attr('width', width).attr('height', height)
+            .attr('transform', `translate(${this.thing_width},0)`);
+        element.append('xhtml:body').html(html);
+        this.things.push({width, height, element, offset: this.thing_width});
+        this.thing_width += this.thing_margin + width;
     }
     resize(width) {
-        this.slider_width;
+        this.slider_width = this.width - label_margin - this.thing_width;
     }
 }
 
 class Thing {
-    constructor() {
-
+    constructor(name) {
+        this.name = name;
+        this.height = slider_height;
     }
 }
 
 class Point extends Thing {
-    constructor() {
-        super();
+    constructor(name) {
+        super(name);
+        this.width = 48;
+        this.x = [];
+        this.y = [];
     }
-    html(row) {
-
+    append(row, index, array) {
+        const x_name = this.name + '-x', y_name = this.name + '-y';
+        const x_id = `${row.symbol}-${x_name}`, y_id = `${row.symbol}-${y_name}`;
+        const x_checked = index == 0 ? 'checked' : '';
+        const y_checked = index == array.length - 1 ? 'checked' : '';
+        const x = `<input type="radio" class="control control-point" id="${x_id}" name="${x_name}" ${x_checked}/>`;
+        const y = `<input type="radio" class="control control-point" id="${y_id}" name="${y_name}" ${y_checked}/>`;
+        const html = x + y;
+        row.add_thing(this.width, this.height, html);
+        this.x.push(document.getElementById(x_id));
+        this.y.push(document.getElementById(y_id));
     }
 }
 
 class Path extends Thing {
-    constructor() {
-        super();
+    constructor(name) {
+        super(name);
+        this.width = 64;
+        this.t = [];
+        this.x = [];
+        this.y = [];
     }
-    html(row) {
-
+    append(row, index, array) {
+        const s = row.symbol;
+        const t_name = this.name + '-t', x_name = this.name + '-x', y_name = this.name + '-y';
+        const t_id = `${s}-${t_name}`, x_id = `${s}-${x_name}`, y_id = `${s}-${y_name}`;
+        const t_checked = index == 0 ? 'checked' : '';
+        const x_checked = index == 0 ? 'checked' : '';
+        const y_checked = index == array.length - 1 ? 'checked' : '';
+        const t = `<input type="radio" class="control control-point" id="${t_id}" name="${t_name}" ${t_checked}/>`;
+        const x = `<input type="radio" class="control control-point" id="${x_id}" name="${x_name}" ${x_checked}/>`;
+        const y = `<input type="radio" class="control control-point" id="${y_id}" name="${y_name}" ${y_checked}/>`;
+        const html = t + x + y;
+        row.add_thing(this.width, this.height, html);
+        this.t.push(document.getElementById(t_id));
+        this.x.push(document.getElementById(x_id));
+        this.y.push(document.getElementById(y_id));
     }
 }
